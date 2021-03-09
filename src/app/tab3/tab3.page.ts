0ee43c509de539619180cf8d788934e1e7e83c42
thuan <thuan.hoang@vastbit.com>
2021-03-09 17:47:48 +0700

    Prototype for Padometer

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { IPedometerData, Pedometer } from '@ionic-native/pedometer/ngx';
 
 @Component({
@@ -7,24 +7,20 @@ import { IPedometerData, Pedometer } from '@ionic-native/pedometer/ngx';
   styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
-  isDistanceAvailable: any;
-  numberOfSteps: number | 0;
+  // private pedometer: any;
+  public isStepCountingAvailable: any;
+  public step: any = { numberOfSteps: 2 };
 
-  constructor(Pedometer: Pedometer) {
-    Pedometer.isDistanceAvailable()
-      .then((available: boolean) => {
-        this.isDistanceAvailable = available;
-        console.log(available);
-      })
-      .catch((error: any) => console.log(error));
-
-    Pedometer.startPedometerUpdates().subscribe(
-      (data: IPedometerData) => {
-        alert(JSON.stringify(data));
-        console.log(data);
-        this.numberOfSteps = data.numberOfSteps;
-      },
-      (err) => alert(JSON.stringify(err))
-    );
+  constructor(private ngZone: NgZone, private Pedometer: Pedometer) {
+    Pedometer.isStepCountingAvailable().then((available: boolean) => {
+      this.isStepCountingAvailable = available;
+    });
+    this.onSubscribePedometer();
   }
+
+  onSubscribePedometer = () => {
+    this.Pedometer.startPedometerUpdates().subscribe((data: IPedometerData) => {
+      this.ngZone.run(() => (this.step = data));
+    });
+  };
 }
